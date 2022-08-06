c778994c9e499805f2b86ff64bbdac534dd319e7
Jose Paredes <jozemparedes@gmail.com>
2022-08-06 00:58:53 -0600

    update func names for printing

--- colors/gradient.py
+++ colors/gradient.py
@@ -3,32 +3,33 @@ from colors.rgb import RGB
 from colors.utils import split_str
 
 class Gradient:
-    steps : RGB =[]
+    step_list : RGB =[]
 
     def len(self)->int:
-        return len(self.colors)
+        return len(self.step_list)
 
-    def add_color(self, color: RGB):
-        self.steps.append(color)
+    def add_step(self, color: RGB):
+        self.step_list.append(color)
 
-    def add_colors(self, *args : RGB):
+    def add_steps(self, *args : RGB):
         for color in args:
-            self.steps.append(color)
+            self.step_list.append(color)
     
     @property
-    def colors(self)-> List:
-        return self.steps
+    def steps(self)-> List:
+        return self.step_list
 
 
 
 
-def pgrad(grad : Gradient, string: str):
+
+def printg(grad : Gradient, string: str):
 
     END_SEQ = f"\x1b[0m"
     s = split_str(string, grad.len())
 
     for i in range(len(s)):
-        c = grad.colors[i]
+        c = grad.steps[i]
         rgb_seq = f"\x1b[38;2;{c.r};{c.g};{c.b}m"
         print(rgb_seq + s[i], end="")    
 
--- colors/rgb.py
+++ colors/rgb.py
@@ -1,5 +1,6 @@
-class RGB:
+from typing import Tuple
 
+class RGB:
 
     def __init__(self, r,g,b):
         self.r = r
@@ -12,15 +13,19 @@ class RGB:
 
 
     @property
-    def codes(self):
+    def codes(self) -> Tuple:
         return (self.r, self.g, self.b)
 
+
     
+def printc(color : RGB, string: str, inline = False):
+    """
+        prints "string" into the terminal in "RGB" color
+    """
 
-
-def pcolor(color : RGB, string: str, inline = False):
     rgb_seq = f"\x1b[38;2;{color.r};{color.g};{color.b}m"
     end_seq = f"\x1b[0m"
+    
     print(rgb_seq, end="")
     print(string, end="")
 
