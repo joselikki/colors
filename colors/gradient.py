e877f239b961a028fd2103efdfc1e3fff764e983
Jose Paredes <jozemparedes@gmail.com>
2022-08-09 17:41:47 -0600

    fix background gradient sequence

--- colors/gradient.py
+++ colors/gradient.py
@@ -20,29 +20,34 @@ class Gradient:
         return self.step_list
 
 def fontgrad(string: str, grad : Gradient) -> str:
-
-    END_SEQ = f"\x1b[0m"
-    s = split_str(string, grad.len())
+   
     output = ""
+    #Spliting the string to apply a different RBG to each chunk
+    s = split_str(string, grad.len())
 
     for i in range(len(s)):
         c = grad.steps[i]
         rgb_seq = f"\x1b[38;2;{c.r};{c.g};{c.b}m"
         output += rgb_seq +s[i]
-    return output + END_SEQ
+
+    return output + "\x1b[0m" # reset ANSII sequence
 
     
 def backgrad(string: str , background: Gradient, fcolor: RGB = None) -> str:
     
-    END_SEQ = f"\x1b[0m"
-    s = split_str(string, background.len())
-    font_col_seq = f"\x1b[38;2;{fcolor.r};{fcolor.g};{fcolor.b}m"
+
+    font_col_seq = ""
     output = ""
+    #Spliting the string to apply a different RBG to each chunk
+    s = split_str(string, background.len())
+
+    if fcolor:
+        font_col_seq = f"\x1b[38;2;{fcolor.r};{fcolor.g};{fcolor.b}m"
 
     for i in range(len(s)):
         c = background.steps[i]
         rgbb_seq = f"\x1b[48;2;{c.r};{c.g};{c.b}m"
-        output = rgbb_seq + s[i]
+        output += rgbb_seq + s[i]
 
 
-    return font_col_seq + output + END_SEQ
+    return font_col_seq + output + "\x1b[0m" # reset ANSII sequence
