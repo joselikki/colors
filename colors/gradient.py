8d20ca4c9f402081a6291fcc7ef57bf2b79f821a
Jose Paredes <jozemparedes@gmail.com>
2022-08-09 17:26:41 -0600

    add background gradient

--- colors/gradient.py
+++ colors/gradient.py
@@ -19,7 +19,7 @@ class Gradient:
     def steps(self)-> List:
         return self.step_list
 
-def gradiently(grad : Gradient, string: str) -> str:
+def fontgrad(string: str, grad : Gradient) -> str:
 
     END_SEQ = f"\x1b[0m"
     s = split_str(string, grad.len())
@@ -29,5 +29,20 @@ def gradiently(grad : Gradient, string: str) -> str:
         c = grad.steps[i]
         rgb_seq = f"\x1b[38;2;{c.r};{c.g};{c.b}m"
         output += rgb_seq +s[i]
-
     return output + END_SEQ
+
+    
+def backgrad(string: str , background: Gradient, fcolor: RGB = None) -> str:
+    
+    END_SEQ = f"\x1b[0m"
+    s = split_str(string, background.len())
+    font_col_seq = f"\x1b[38;2;{fcolor.r};{fcolor.g};{fcolor.b}m"
+    output = ""
+
+    for i in range(len(s)):
+        c = background.steps[i]
+        rgbb_seq = f"\x1b[48;2;{c.r};{c.g};{c.b}m"
+        output = rgbb_seq + s[i]
+
+
+    return font_col_seq + output + END_SEQ
